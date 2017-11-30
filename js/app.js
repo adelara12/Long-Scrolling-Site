9e5f5323486667f5d71750c923b1b7d48694d4db
adelara12 <32181573+adelara12@users.noreply.github.com>
2017-11-29 23:43:37 -0800

    jlkjl

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var canvas= document.getElementById("canvas");
+var counter= document.getElementById("counter");
+
+window.onscroll = function(){
+
+  var y = window.pageYOffset;
+
+    console.log(y);
+
+    counter.style.top = 400 + y + .97 + "px";
+
+    counter.innerHTML = y + "px and also" + 400 + y + .97 + "px";
+
+    if(y>2000){counter.style.backgroundColor = "green";}
+    if(y<2000){counter.style.backgroundColor = "blue";}
+
+  };
